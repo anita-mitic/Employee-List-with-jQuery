6ebeed69a7467eb8fe61c0784f4cae189546c887
anita-mitic <mitic_anita@yahoo.com>
2017-12-26 22:14:09 +0100

    Initial commit

---
+++ js/widget.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $.getJSON('../data/employees.json', function (data) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
+      } else {
+        statusHTML +='<li class="out">';
+      }
+      statusHTML += employee.name + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+  
+  $.getJSON('../data/rooms.json', function (data) {
+    var roomsHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        roomsHTML +='<li class="empty">';
+      } else {
+        roomsHTML +='<li class="full">';
+      }
+      roomsHTML += room.room + '</li>';
+    });
+    roomsHTML += '</ul>';
+    $('#roomList').html(roomsHTML)
+  }); // end getJSON
+}); // end ready
+
+  
